3f8dd2c9b9d53684bfbb93e23b884b0db2644b4f
François-Xavier Guillois <internet@fxguillois.email>
2024-03-08 15:36:52 +0100

    l'ajout de command est à nouveau fonctionnel :)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,61 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-// const { updateEspansoConfig } = require('./espansoConfig');
-const { commandsListData, command1Action } = require('./commands.mjs');
- 
-// Initialize Discord Bot
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-/**
- * Registers the 'push-prompt' slash command and adds it globally when 
- * the bot is ready. The command allows sending a prompt with trigger
- * and content to the espanso prompt queue.
- */
-client.once('ready', async () => {
-    console.log('Enrole bot ready !', `Logged in as ${client.user.tag}!`);
- 
-    // Adding the command globally
-    await client.application.commands.create(commandsListData);
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, async readyClient => {
+	console.log(`Enrole BOT Ready! Logged in as ${readyClient.user.tag}`);
+
+	try {
+		// Loop through all the commands and register them with Discord's API
+		for (const command of client.commands.values()) {
+		  await client.application.commands.create(command.data);
+		  console.log(`[INFO] Registered command ${command.data.name}`);
+		}
+	  } catch (error) {
+		console.error(error);
+	  }
 });
 
-/**
- * Handles interactions from Discord slash commands.
- * If the interaction is from the 'auto-prompt' command, 
- * extract the url from the options, 
- * get the message content and send it to API
-*/
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    // when the interaction is from the 'auto-prompt' command, extract the url from the options, get the message content and send it to API
-    if (interaction.commandName === 'auto-prompt') {
-        command1Action(interaction);
-    }
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	console.log(`[INFO] Received command ${interaction.commandName}`);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction, client);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
